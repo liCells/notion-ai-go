cfaf39152715d0b744d5674bd561558c2f497cf2
LZ <lizong1204@gmail.com>
2023-05-03 20:31:30 +0800

    refactor: optimize

--- main.go
+++ main.go
@@ -1,37 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "github.com/jyz0309/notionAI-go/client"
-    "os"
+	"fmt"
+	"github.com/jyz0309/notionAI-go/client"
+	"os"
 )
 
 func main() {
-    args := os.Args
-    if len(args) != 6 {
-        fmt.Println("Usage: <option> <token> <spaceId> <prompt> <proxy>")
-        return
-    }
-    execute(args[1], args[2], args[3], args[4], args[5])
+	args := os.Args
+	if len(args) == 5 {
+		cli := client.NewClient(args[2], args[3])
+		execute(args[1], args[4], cli)
+		return
+	}
+	if len(args) == 6 {
+		cli := client.NewProxyClient(args[2], args[3], args[5])
+		execute(args[1], args[4], cli)
+		return
+	}
+	fmt.Println("Usage: <option> <token> <spaceId> <prompt> <proxy>")
 }
 
-func execute(option string, token string, spaceId string, q string, proxy string) {
-    cli := client.NewProxyClient(token, spaceId, proxy)
-    var val = ""
-    var err error
-    switch option {
-    case "draft":
-        val, err = cli.HelpMeDraft(q)
-    case "translate_into_chinese":
-        val, err = cli.HelpMeEdit(q, "Translate into Chinese")
-    case "translate_into_english":
-        val, err = cli.HelpMeEdit(q, "Translate into English")
-    case "continue":
-        val, err = cli.ContinueWriting(q)
-    }
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(val)
+func execute(option string, q string, cli *client.NotionClient) {
+	var val = ""
+	var err error
+	switch option {
+	case "draft":
+		val, err = cli.HelpMeDraft(q)
+	case "translate_into_chinese":
+		val, err = cli.HelpMeEdit(q, "Translate into Chinese")
+	case "translate_into_english":
+		val, err = cli.HelpMeEdit(q, "Translate into English")
+	case "continue":
+		val, err = cli.ContinueWriting(q)
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(val)
 }
